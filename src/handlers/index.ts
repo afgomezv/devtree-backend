9581ef44f9543247dad29cbdef31c138e0cd644a
afgomezv <afgomezvonline@gmail.com>
2024-11-28 22:33:09 -0500

    cors config

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { validationResult } from "express-validator";
 import User from "../models/User";
 import { checkPassword, hashPassword } from "../utils/auth";
 import { generateUniqueSlug } from "../utils/slug";
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config.js";
 import router from "./router";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
+
+connectDB();
 
 const app = express();
-connectDB();
+
+//* Cors
+app.use(cors(corsConfig));
 
 //* Leer datos de los formularios.
 app.use(express.json());
